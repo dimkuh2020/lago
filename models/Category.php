2ccfca65f208970ec7e3d49ef48815e8e8390d49
dimkuh <dimkuh1985@gmail.com>
2020-01-23 17:41:17 +0200

    заготовка для категорий(вид/тема) + виджет на меню

---
+++ models/Category.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Category extends ActiveRecord{
+
+    public static function tableName(){
+        return 'category'; //название табл
+    }
+
+    public function getProducts(){
+        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+    }
+
+
+}
+
+?>
--- views/category/index.php
+++ views/category/index.php
@@ -1,67 +1,16 @@
 <?php
+use yii\helpers\Html;
+use yii\helpers\Url; // подкл хелпер для картинок и т.д.
+/* @var $this yii\web\View */
+?>
+<?php
 
 /* @var $this yii\web\View */
 
 $this->title = 'LaGo';
 ?>
 	</div>
-		<div class="container">
-			<div class="head-top">
-				<div class="logo">
-					<a href="index.html"><img src="images/logo1.png" style="width: 80px; height: 80px;" alt=""></a>	
-				</div>
-				<div class=" h_menu4">
-						<ul class="memenu skyblue">
-							<li class="active grid"><a class="color8" href="index.html">Главная</a></li>	
-							<li><a class="color1" href="#">Все товары</a></li>
-							<li class="grid"><a class="color2" href="#">Категории товаров</a>
-								<div class="mepanel">
-								<div class="row">
-									<div class="col1">
-										<div class="h_nav">
-											<ul>
-												<li><a href="products.html">Accessories</a></li>
-												<li><a href="products.html">Bags</a></li>
-												<li><a href="products.html">Caps & Hats</a></li>
-												<li><a href="products.html">Hoodies & Sweatshirts</a></li>
-												<li><a href="products.html">Jackets & Coats</a></li>
-												<li><a href="products.html">Jeans</a></li>
-												<li><a href="products.html">Jewellery</a></li>
-												<li><a href="products.html">Jumpers & Cardigans</a></li>
-												<li><a href="products.html">Leather Jackets</a></li>
-												<li><a href="products.html">Long Sleeve T-Shirts</a></li>
-												<li><a href="products.html">Loungewear</a></li>
-											</ul>	
-										</div>							
-									</div>
-									<div class="col1">
-										<div class="h_nav">
-											<ul>
-												<li><a href="products.html">Shirts</a></li>
-												<li><a href="products.html">Shoes, Boots & Trainers</a></li>
-												<li><a href="products.html">Shorts</a></li>
-												<li><a href="products.html">Suits & Blazers</a></li>
-												<li><a href="products.html">Sunglasses</a></li>
-												<li><a href="products.html">Sweatpants</a></li>
-												<li><a href="products.html">Swimwear</a></li>
-												<li><a href="products.html">Trousers & Chinos</a></li>
-												<li><a href="products.html">T-Shirts</a></li>
-												<li><a href="products.html">Underwear & Socks</a></li>
-												<li><a href="products.html">Vests</a></li>
-											</ul>	
-										</div>							
-									</div>									
-								</div>
-							</div>
-						</li>
-						<li><a class="color4" href="blog.html">Наш блог</a></li>				
-						<li><a class="color6" href="contact.html">Написать нам</a></li>
-					</ul> 
-				</div>				
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>	
+		<?=\app\components\MenuWidget::widget(['tpl' => 'menu'])?>
 	<div class="banner">
 		<div class="container">			   
 			<div  id="top" class="callbacks_container">
@@ -98,7 +47,7 @@ $this->title = 'LaGo';
 		<h1>NEW RELEASED</h1>
 		<div class="grid-in">
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>T-Shirt</span>	
@@ -108,7 +57,7 @@ $this->title = 'LaGo';
 				<p><a href="single.html">Contrary to popular</a></p>
 			</div>
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi1.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi1.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>Shoe</span>	
@@ -118,7 +67,7 @@ $this->title = 'LaGo';
 			<p><a href="single.html">classical Latin</a></p>
 			</div>
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi2.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi2.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>Bag</span>	
@@ -131,7 +80,7 @@ $this->title = 'LaGo';
 		</div>
 		<div class="grid-in">
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi3.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi3.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>Shirt</span>	
@@ -141,7 +90,7 @@ $this->title = 'LaGo';
 			<p><a href="single.html">suffered alteration</a></p>
 			</div>
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi4.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi4.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>Bag</span>	
@@ -151,7 +100,7 @@ $this->title = 'LaGo';
 			    <p><a href="single.html">Content here</a></p>
 			</div>
 			<div class="col-md-4 grid-top">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/pi5.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/pi5.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-left    b-delay03 ">
 							<span>Shoe</span>	
@@ -167,7 +116,7 @@ $this->title = 'LaGo';
 	<div class="content-top-bottom">
 		<h2>Featured Collections</h2>
 		<div class="col-md-6 men">
-			<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t1.jpg" alt="">
+			<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/t1.jpg" alt="">
 				<div class="b-wrapper">
 					<h3 class="b-animate b-from-top top-in   b-delay03 ">
 						<span>Lorem</span>	
@@ -177,7 +126,7 @@ $this->title = 'LaGo';
 		</div>
 		<div class="col-md-6">
 			<div class="col-md1 ">
-				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t2.jpg" alt="">
+				<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/t2.jpg" alt="">
 					<div class="b-wrapper">
 						<h3 class="b-animate b-from-top top-in1   b-delay03 ">
 							<span>Lorem</span>	
@@ -187,7 +136,7 @@ $this->title = 'LaGo';
 			</div>
 			<div class="col-md2">
 				<div class="col-md-6 men1">
-					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t3.jpg" alt="">
+					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/t3.jpg" alt="">
 						<div class="b-wrapper">
 							<h3 class="b-animate b-from-top top-in2   b-delay03 ">
 								<span>Lorem</span>	
@@ -196,7 +145,7 @@ $this->title = 'LaGo';
 					</a>
 				</div>
 				<div class="col-md-6 men2">
-					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t4.jpg" alt="">
+					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="/images/t4.jpg" alt="">
 						<div class="b-wrapper">
 							<h3 class="b-animate b-from-top top-in2   b-delay03 ">
 								<span>Lorem</span>	
