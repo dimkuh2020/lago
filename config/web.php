3162563a1daa2e4cbb56f771ce919f833418daa9
dimkuh <dimkuh1985@gmail.com>
2020-02-10 18:23:02 +0200

    корзина v.2

--- config/web.php
+++ config/web.php
@@ -52,7 +52,8 @@ $config = [
             'rules' => [
                 'category/<id:\d+>/page/<page:\d+>' => 'category/view', //ЧПУ для пагинации (ставим перед правилом для категорий)
                 'category/<id:\d+>' => 'category/view', // Показ цифру id после категории
-                'product/<id:\d+>' => 'product/view', 
+                'product/<id:\d+>' => 'product/view',
+                'search' => 'category/search', 
 
             ],
         ],       
